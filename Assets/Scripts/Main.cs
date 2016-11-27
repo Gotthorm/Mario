6fe3f269edefdd47e47a3970ea7603441689588c
Sean Butterworth <sean_butterworth@hotmail.com>
2016-11-26 21:31:40 -0800

    Update
    Created a new boot scene to test changing screen resolutions, etc.
    Some more work on the iPhone emulator scene also

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,15 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class Main : MonoBehaviour {
-
+public class Main : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
+        // Load preferences
+        m_DesiredWindowWidth = PlayerPrefs.GetInt(m_kSettingPrefix + m_kSettingWindowWidth, m_DefaultWindowWidth);
+        m_DesiredWindowHeight = PlayerPrefs.GetInt(m_kSettingPrefix + m_kSettingWindowHeight, m_DefaultWindowHeight);
+        m_FullScreen = (PlayerPrefs.GetInt(m_kSettingPrefix + m_kSettingFullScreen, m_DefaultFullScreen ? 1 : 0) == 1);
+
+        Debug.Log("Loaded preference: desiredWidth(" + m_DesiredWindowWidth.ToString() + ")");
+        Debug.Log("Loaded preference: desiredHeight(" + m_DesiredWindowHeight.ToString() + ")");
+        Debug.Log("Loaded preference: FullScreen(" + m_FullScreen.ToString() + ")");
+
+        m_CurrentWindowWidth = (m_DesiredWindowWidth < 0) ? Screen.currentResolution.width : m_DesiredWindowWidth;
+        m_CurrentWindowHeight = (m_DesiredWindowHeight < 0) ? Screen.currentResolution.height : m_DesiredWindowHeight;
+
+        Debug.Log("Current target window dimensions: " + m_CurrentWindowWidth.ToString() + "X" + m_CurrentWindowHeight.ToString());
+
+        // The returned resolutions are sorted by width, lower resolutions come first
+        Resolution[] resolutionList = Screen.resolutions;
+
+        bool isCurrentResolutionValid = false;
+        List<string> ResolutionStringList = new List<string>();
+        foreach (Resolution resolution in resolutionList)
+        {
+            Debug.Log(resolution.ToString());
+            ResolutionStringList.Add(resolution.width.ToString() + "X" + resolution.height.ToString());
+
+            if (resolution.width == m_CurrentWindowWidth && resolution.height == m_CurrentWindowHeight)
+            {
+                isCurrentResolutionValid = true;
+
+                // When logging the entries, always finish iterating the list
+                //break;
+            }
+        }
+
+        if(isCurrentResolutionValid == false)
+        {
+            // If this happens the saved preferences might be invalid.
+            // Use the default settings and reset the p[references.
+            m_CurrentWindowWidth = Screen.width;
+            m_CurrentWindowHeight = Screen.height;
+            m_FullScreen = m_DefaultFullScreen;
+            m_DesiredWindowWidth = m_DefaultWindowWidth;
+            m_DesiredWindowHeight = m_DefaultWindowHeight;
+        }
+
+        Debug.Log("Attempting window dimensions: " + m_CurrentWindowWidth.ToString() + "X" + m_CurrentWindowHeight.ToString());
+
+        Screen.SetResolution(m_CurrentWindowWidth, m_CurrentWindowHeight, m_FullScreen);
+
+        m_ResolutionDropdown = GameObject.Find("Dropdown_ScreenResolution").GetComponent<Dropdown>();
+
+        if(m_ResolutionDropdown)
+        {
+            m_ResolutionDropdown.ClearOptions();
+            m_ResolutionDropdown.AddOptions(ResolutionStringList);
+            //m_ResolutionDropdown.options.Add(new Dropdown.OptionData("FartyPants"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Use this to clean up
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt(m_kSettingPrefix + m_kSettingWindowWidth, m_DesiredWindowWidth);
+        PlayerPrefs.SetInt(m_kSettingPrefix + m_kSettingWindowHeight, m_DesiredWindowHeight);
+        PlayerPrefs.SetInt(m_kSettingPrefix + m_kSettingFullScreen, m_FullScreen ? 1 : 0);
+    }
+
+    // Settings
+    private int m_DesiredWindowWidth = -1;
+    private int m_DesiredWindowHeight = -1;
+    private int m_CurrentWindowWidth = 0;
+    private int m_CurrentWindowHeight = 0;
+    private bool m_FullScreen = false;
+
+    // Just some name to ensure we do not conflict with system settings
+    private const string m_kSettingPrefix = "Mario_";
+
+    private const string m_kSettingWindowWidth = "WindowWidth";
+    private const string m_kSettingWindowHeight = "WindowHeight";
+    private const string m_kSettingFullScreen = "FullScreen";
+
+    // Window width and height use the value of -1 to indicate "current"
+    private const int m_DefaultWindowWidth = -1;
+    private const int m_DefaultWindowHeight = -1;
+    private const bool m_DefaultFullScreen = true;
+
+    private Dropdown m_ResolutionDropdown = null;
 }
--- Assets/Scripts/iPhoneEmulator.cs
+++ Assets/Scripts/iPhoneEmulator.cs
@@ -16,13 +16,41 @@ public class iPhoneEmulator : MonoBehaviour
         m_Button_4  = GameObject.Find("Button_iPhone4").GetComponent<Button>();
 
         EnableButton(m_Button_7S);
+
+        m_WindowWidth = GameObject.Find("Text_MainWindowWidth").GetComponent<Text>();
+        m_WindowHeight = GameObject.Find("Text_MainWindowHeight").GetComponent<Text>();
+        m_iPhoneWidth = GameObject.Find("Text_iPhoneWindowWidth").GetComponent<Text>();
+        m_iPhoneHeight = GameObject.Find("Text_iPhoneWindowHeight").GetComponent<Text>();
+
+        m_MainWindowRect = GameObject.Find("Canvas_Main").GetComponent<RectTransform>();
+
+        if(m_MainWindowRect)
+        {
+            m_MainWindowSize = new Vector2(m_MainWindowRect.rect.width, m_MainWindowRect.rect.height);
+            UpdateWindowSize(m_MainWindowRect.rect.width, m_MainWindowRect.rect.height);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-	
-	}
+        if (m_MainWindowRect)
+        {
+            if(m_MainWindowSize.x != m_MainWindowRect.rect.width || m_MainWindowSize.y != m_MainWindowRect.rect.height )
+            {
+                UpdateWindowSize(m_MainWindowRect.rect.width, m_MainWindowRect.rect.height);
+            }
+        }
+
+        if (m_iPhoneWidth)
+        {
+            m_iPhoneWidth.text = "fart";
+        }
+        if (m_iPhoneHeight)
+        {
+            m_iPhoneHeight.text = "fart";
+        }
+    }
 
     public void OnClick_iPhone7S()
     {
@@ -60,6 +88,20 @@ public class iPhoneEmulator : MonoBehaviour
         EnableButton(m_Button_4);
     }
 
+    private void UpdateWindowSize( float width, float height )
+    {
+        m_MainWindowSize = new Vector2(width, height);
+
+        if (m_WindowWidth)
+        {
+            m_WindowWidth.text = width.ToString();
+        }
+        if (m_WindowHeight)
+        {
+            m_WindowHeight.text = height.ToString();
+        }
+    }
+
     // .5625 is 16:9
 
     private void DisableCurrentButton()
@@ -91,4 +133,12 @@ public class iPhoneEmulator : MonoBehaviour
     private Button m_Button_6 = null;
     private Button m_Button_5 = null;
     private Button m_Button_4 = null;
+
+    private Text m_WindowWidth = null;
+    private Text m_WindowHeight = null;
+    private Text m_iPhoneWidth = null;
+    private Text m_iPhoneHeight = null;
+
+    private RectTransform m_MainWindowRect = null;
+    private Vector2 m_MainWindowSize;
 }
